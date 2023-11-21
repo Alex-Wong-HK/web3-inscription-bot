af5f1e663c6a9530eca601a5f7bbc020e5e66dd0
alex <alex.wong@one2.cloud>
2023-11-21 11:41:47 +0800

    update python version

--- src/python/inscripton.py
+++ src/python/inscripton.py
@@ -1,43 +1,83 @@
-from web3 import Web3
+from web3 import Web3, HTTPProvider, Account
+from web3.middleware import geth_poa_middleware
 
-address = 'walletAddress'
-_p = 'privateKeyhere'
-rpc_url = 'https://1rpc.io/avax/c'
-web3 = Web3(Web3.HTTPProvider(rpc_url))
-print(web3.is_connected())
-print(Web3.from_wei(web3.eth.get_balance(address),'ether'))
-c=0
-_d_t = b'data:,{"p":"asc-20","op":"mint","tick":"aval","amt":"100000000"}'
-_d = '0x646174613a2c7b2270223a226173632d3230222c226f70223a226d696e74222c227469636b223a226176616c222c22616d74223a22313030303030303030227d'
+private_key = "private key"
+recipient_address = "wallet address"
+data = 'data:,{"p":"prc-20","op":"mint","tick":"pols","amt":"100000000"}'
+rpc_map = {
+    'mainnet': 'rpc_link',
+}
 
-while True:
-    print(Web3.from_wei(web3.eth.get_balance(address),'ether'))
-    nonce = web3.eth.get_transaction_count(address)
-    tx = {
+
+def get_transaction_eip1559(rpc_url, text_data, priority_fee):
+    web3 = Web3(HTTPProvider(rpc_url))
+    web3.middleware_onion.inject(geth_poa_middleware, layer=0)  # Inject POA middleware
+    # Check if connected to Ethereum network
+    if not web3.isConnected():
+        raise Exception("Not connected to Ethereum network")
+    # Set up the sender's account
+    sender_account = Account.from_key(private_key)
+    sender_address = sender_account.address
+
+    # Transaction details
+    value = web3.toWei(0, 'ether')
+    # Get the nonce for the sender's account
+    nonce = web3.eth.getTransactionCount(sender_address, 'pending')
+    base_fee = web3.eth.get_block('latest')['baseFeePerGas']
+
+    # Transaction details
+    max_priority_fee_per_gas = web3.toWei(priority_fee, 'gwei')  # Tip for the miner, adjust as needed
+    max_fee_per_gas = base_fee + max_priority_fee_per_gas
+    # EIP-1559 transaction parameters
+
+    # Convert data to hex and add as data to the transaction
+    data_hex = web3.toHex(text=text_data)
+
+    # Estimate gas limit for the transaction
+    gas_estimate = web3.eth.estimate_gas({
+        'to': recipient_address,
+        'value': value,
+        'from': sender_address,
+        'data': data_hex
+    })
+
+    # Create the transaction dictionary
+    transaction = {
+        'type': '0x2',  # Indicates an EIP-1559 transaction
+        'chainId': web3.eth.chain_id,
         'nonce': nonce,
-        'chainId': 43114,
-        'to': address,
-        'from':address,
-        'data':_d,
-        'maxFeePerGas': Web3.to_wei(195, 'gwei'),
-        'maxPriorityFeePerGas': Web3.to_wei(18, 'gwei'),
-        # 'gas': 24000,
-        'value': Web3.to_wei(0, 'ether')
+        'maxPriorityFeePerGas': max_priority_fee_per_gas,
+        'maxFeePerGas': max_fee_per_gas,
+        'gas': gas_estimate,
+        'to': recipient_address,
+        'value': value,
+        'data': data_hex
     }
-    try:
-        gas = web3.eth.estimate_gas(tx)
-        tx['gas'] = gas
-        print(tx)
-        signed_tx = web3.eth.account.sign_transaction(tx,_p)
-        tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-        print(web3.to_hex(tx_hash))
-        receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
-        print("done", receipt)
-        if receipt.status == 1:
-            c = c+1
-            print("%s Mint Success!" %c)
-            continue
-        else:
-            continue
-    except Exception as e:
-        print(e)
+    print(f"Transaction: {transaction}")
+    return transaction, web3, private_key, nonce
+
+
+def signed_send(transaction, web3, private_key, is_wait=True):
+    # Sign the transaction
+    signed_tx = web3.eth.account.sign_transaction(transaction, private_key)
+    # Send the transaction
+    tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    # Get the transaction hash
+    print(f"Transaction hash: {tx_hash.hex()}")
+    # Wait for the transaction receipt (optional)
+    if is_wait:
+        tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
+        print(f"Transaction receipt: {tx_receipt}")
+        print(f"Transaction status: {tx_receipt['status']}")
+
+
+def send_transaction(number, rpc, test_data, is_wait=True, priority_fee=400):
+    transaction, web3, private_key, nonce = get_transaction_eip1559(rpc, test_data, priority_fee)
+    for i in range(number):
+        transaction.update({'nonce': nonce})
+        signed_send(transaction, web3, private_key, is_wait)
+        nonce = nonce + 1
+
+
+if __name__ == '__main__':
+    send_transaction(1, rpc_map.get("mainnet"), data, is_wait=True, priority_fee=100)
